f89033234b2c716b08fff26a99b711b594921c12
akashabdulmazid <akashabdulmazid@gmail.com>
2026-01-06 22:08:29 +0600

    conceptual session first video done

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import app from "./app";
+import { prisma } from "./lib/prisma";
+
+const port = process.env.PORT || 3000;
+
+async function server() {
+    try {
+        await prisma.$connect();
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        })
+    } catch (error) {
+        console.error(error);
+        await prisma.$disconnect();
+        process.exit(1);
+    }
+};
+
+
+server();
