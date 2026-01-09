aceb6f98b8f9d8fdf1f4dadf83da307cf6ed7e3a
akashabdulmazid <akashabdulmazid@gmail.com>
2026-01-09 17:14:39 +0600

    conceptual session prisma part 2 complete

---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,17 @@
+import { RequestHandler } from "express";
+import { prisma } from "../../lib/prisma";
+
+const register: RequestHandler = async (req, res)=>{
+    const payload = req.body;
+
+    const user = await prisma.user.create({
+        data: payload,
+    });
+
+    res.send({message: "Registered user successfully", data: user});
+};
+
+
+export const userController = {
+    register
+}
